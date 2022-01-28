02a606233a2b8de8b072d9d77754c625cb082756
Cédric Allain <CedricAllainEnsae@users.noreply.github.com>
2022-01-28 14:29:38 +0100

    update with plot general figure (atom in class + mean atom)

--- mean_atom.py
+++ mean_atom.py
@@ -171,12 +171,18 @@ def compute_mean_atom(df_topomaps, use_batch_cdl):
     }
 
     def procedure(class_label):
+        new_rows = []
+
         for method in ['cah', 'kmeans']:
             # Reconstruct signal for a given class
             X, info, n_times_atom = reconstruct_class_signal(
                 df_topomaps, method, label=class_label)
             cdl_params['n_times_atom'] = n_times_atom
             cdl_model, _ = run_csc(X, **cdl_params)
+            # append dataframe
+            new_rows.append({'class_label': class_label,
+                             'method': method,
+                             'u_hat': cdl_model.u_hat_[0]})
             # plot "mean" atom
             mne.viz.plot_topomap(data=cdl_model.u_hat_[
                                  0], pos=info, show=False)
@@ -188,8 +194,76 @@ def compute_mean_atom(df_topomaps, use_batch_cdl):
             plt.savefig('results_mean_atom/' + fig_name + '.png')
             plt.close()
 
-    Parallel(n_jobs=6, verbose=1)(
+        return new_rows
+
+    new_rows = Parallel(n_jobs=6, verbose=1)(
         delayed(procedure)(class_label) for class_label in range(n_clusters))
+
+    df_mean = pd.DataFrame()
+    for new_row in new_rows:
+        df_mean = df_mean.append(new_row, ignore_index=True)
+
+    return df_mean
+
+
+def plot_global_class(df, df_mean=None, method='cah', class_label=0):
+    """
+
+    """
+    sub_df = df[df['labels_' + method] == class_label]
+    subjects = set(sub_df['subject_id'].values)
+    n_atoms = sub_df.shape[0]
+
+    if df_mean is not None:
+        u_mean = df_mean[(df_mean['method'] == method) & (
+            df_mean['class_label'] == class_label)]['u_hat'].values[0]
+        plot_mean = True
+    else:
+        plot_mean = False
+
+    # plot topomaps
+    n_columns = min(6, n_atoms)
+    split = int(np.ceil(n_atoms / n_columns))
+    if (n_atoms % n_columns) in [0, (n_columns - 1)]:
+        split += 1
+    figsize = (4 * n_columns, 3 * split)
+    fig, axes = plt.subplots(split, n_columns, figsize=figsize)
+    axes = np.atleast_2d(axes)
+
+    for ii in range(n_atoms):
+        row = sub_df.iloc[ii]
+        subject_id, atom_id = row['subject_id'], int(row['atom_id'])
+
+        # get CDL results for the subject_id
+        file_name = RESULTS_DIR / subject_id / 'CSCraw_0.5s_20atoms.pkl'
+        cdl_model, info, _, _ = pickle.load(open(file_name, "rb"))
+        u_hat_ = cdl_model.u_hat_
+        # select only grad channels
+        meg_indices = mne.pick_types(info, meg='grad')
+        info = mne.pick_info(info, meg_indices)
+
+        i_row, i_col = ii // n_columns, ii % n_columns
+        it_axes = iter(axes[i_row:(i_row + 1), i_col])
+
+        ax = next(it_axes)
+        ax.set_title('Atom % d Subject %s' % (atom_id, subject_id), pad=0)
+
+        mne.viz.plot_topomap(
+            data=u_hat_[atom_id], pos=info, axes=ax, show=False)
+
+    if plot_mean:
+        # plot mean atom
+        ax = axes[-1, -1]
+        ax.set_title('Mean atom', pad=0)
+        mne.viz.plot_topomap(
+            data=u_mean, pos=info, axes=ax, show=False)
+
+    # save figure
+    fig.tight_layout()
+    fig_name = 'global_figure_age_group_' + str(AGE_GROUP) + "_" + \
+        method + "_class_" + str(class_label)
+    plt.savefig('results_mean_atom/' + fig_name + '.pdf')
+    plt.show()
 
 
 # For each subject, we create the atom clustering based on topomaps correlation
@@ -205,18 +279,10 @@ plt.show()
 n_clusters = 6
 # compute clustering on topomaps
 df_topomaps = df_culstering(df_topomaps, data_columns, n_clusters)
+df_mean = compute_mean_atom(df_topomaps, use_batch_cdl=False)
 
-# For a given class, plot all topomaps accross the subjects
-method = 'cah'
-class_label = 5
-sub_df = df_topomaps[df_topomaps['labels_' + method] == class_label]
-for subject_id in list(set(sub_df['subject_id'].values)):
-    print('subject id %s' % subject_id)
-    atom_idx = sub_df[sub_df['subject_id'] ==
-                      subject_id]['atom_id'].values.astype(int)
-    plot_topomaps(subject_id, atom_idx)
-
-# %%
-compute_mean_atom(df_topomaps, use_batch_cdl=False)
+for method in ['cah', 'kmeans']:
+    for class_label in range(n_clusters):
+        plot_global_class(df_topomaps, df_mean, method, class_label)
 
 # %%

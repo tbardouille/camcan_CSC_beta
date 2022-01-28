2b1317716c3cb5484f15a4ffe236a61d777ad3ad
lindseypower <lindseynp123@gmail.com>
2022-01-28 12:11:00 -0400

    Add files via upload
    Updated to run on full CamCAN dataset

--- correlation_clustering_allAtoms.py
+++ correlation_clustering_allAtoms.py
@@ -1,18 +1,38 @@
+
 import pickle
 import numpy as np
 import pandas as pd
+import scipy.signal as ss
+import os
+import random
 
 #Set thresholds for current run
-thresholds = [0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
+thresholds = [0.4]
+
+#Find full list of subjects from directory 
+subDir = '/media/NAS/lpower/CSC/results'
+subjects = next(os.walk(subDir))[1]
+
+#Exclude ones that have too many similar atoms (previous step)
+exclude_subs = ['CC420061','CC121397','CC420396','CC420348','CC320850','CC410325','CC121428','CC110182','CC420167','CC420261',
+                'CC322186','CC220610','CC221209','CC220506','CC110037','CC510043','CC621642', 'CC521040','CC610052','CC520517', 
+                'CC610469','CC720497','CC610292','CC620129','CC620490']
+for x in exclude_subs: 
+    print(x)
+    subjects.remove(x)
+
+#Choose a random sample of 50 to run code on 
+#sub_list = random.choices(subjects, k=50)
 
 
-subjects = ['CC110606','CC120166','CC120313','CC120727','CC120640','CC210657','CC220518','CC220999','CC221107',
-            'CC220107','CC222125','CC223286','CC310256','CC320089','CC320429','CC320621','CC321000','CC321431','CC321899',
-            'CC410287','CC420060','CC420157','CC420217','CC420566','CC420729','CC510304','CC510438','CC520083',
-            'CC520215','CC520503','CC520597','CC520980','CC610061','CC610288','CC610576','CC620114','CC620264','CC620885',
-            'CC710088','CC710342','CC710664','CC720290','CC721052','CC721504','CC722891','CC723395']
+#sub_list = ['CC110606','CC120166','CC120313','CC120727','CC120640','CC210657','CC220518','CC220999','CC221107',
+#            'CC220107','CC222125','CC223286','CC310256','CC320089','CC320429','CC320621','CC321000','CC321431','CC321899',
+#            'CC410287','CC420060','CC420157','CC420217','CC420566','CC420729','CC510304','CC510438','CC520083',
+#            'CC520215','CC520503','CC520597','CC520980','CC610061','CC610288','CC610576','CC620114','CC620264','CC620885',
+#            'CC710088','CC710342','CC710664','CC720290','CC721052','CC721504','CC722891','CC723395']
 
 u_vector_list = []
+v_vector_list = []
 psd_list = []
 count = 0
 
@@ -30,9 +50,12 @@ for subID in subjects:
 
         u_hat = cdl_model.u_hat_[atom]
         u_vector_list.append(u_hat)
+        
+        v_hat = cdl_model.v_hat_[atom]
+        v_vector_list.append(v_hat)
 
-        psd = np.abs(np.fft.rfft(cdl_model.v_hat_[atom], n=256)) ** 2
-        psd_list.append(psd)
+        #psd = np.abs(np.fft.rfft(cdl_model.v_hat_[atom], n=256)) ** 2
+        #psd_list.append(psd)
     
         #Update the dataframe and increment count 
         dfDict = {'Subject ID': subID, 'Atom number': atom, 'Index': count}
@@ -41,16 +64,25 @@ for subID in subjects:
 
 #Calculate the correlation coefficient between all atoms 
 u_vector_list = np.asarray(u_vector_list)
-psd_list = np.asarray(psd_list)
+v_vector_list = np.asarray(v_vector_list)
 
-u_coefs = np.corrcoef(u_vector_list,u_vector_list)[0:920][0:920]
-psd_coefs = np.corrcoef(psd_list, psd_list)[0:920][0:920]
+v_coefs = []
+for v in v_vector_list:
+    for v2 in v_vector_list: 
+        coef = np.max(ss.correlate(v,v2))
+        v_coefs.append(coef)
+v_coefs = np.asarray(v_coefs)
+v_coefs = np.reshape(v_coefs, (10760,10760))
+
+u_coefs = np.corrcoef(u_vector_list,u_vector_list)[0:10760][0:10760]
+
+threshold_summary = pd.DataFrame(columns=['Threshold', 'Number of Groups', 'Number of Top Groups'])
 
 for thresh in thresholds: 
 
     #Set parameters
     u_thresh = thresh
-    psd_thresh = thresh
+    v_thresh = thresh
 
     atomNum = 0
     groupNum = 0
@@ -75,29 +107,29 @@ for thresh in thresholds:
             gr_atoms = atomGroups[atomGroups['Group number'] == group]
             inds = gr_atoms['Index'].tolist()
             u_groups = []
-            psd_groups = []
+            v_groups = []
      
             #Find the u vector and correlation coefficient comparing the current atom to each atom in the group
             for ind2 in inds:
                 u_coef = u_coefs[ind][ind2]
                 u_groups.append(u_coef)
      
-                psd_coef = psd_coefs[ind][ind2]
-                psd_groups.append(psd_coef)
+                v_coef = v_coefs[ind][ind2]
+                v_groups.append(v_coef)
      
             #average across u and psd correlation coefficients in that group
             u_groups = abs(np.asarray(u_groups))
             avg_u = np.mean(u_groups)
      
-            psd_groups = abs(np.asarray(psd_groups))
-            avg_psd = np.mean(psd_groups)
+            v_groups = abs(np.asarray(v_groups))
+            avg_v = np.mean(v_groups)
 
             #check if this group passes the thresholds 
-            if (avg_u > u_thresh) & (avg_psd > psd_thresh):
+            if (avg_u > u_thresh) & (avg_v > v_thresh):
                 unique = False
                 #If it does, also check if this is the highest cumulative correlation so far
-                if (avg_u + avg_psd) > max_corr:
-                    max_corr = (avg_u + avg_psd)
+                if (avg_u + avg_v) > max_corr:
+                    max_corr = (avg_u + avg_v)
                     max_group = group
      
         #If the atom was similar to at least one group, sorts it into the group that it had the highest cumulative correlation to 
@@ -148,13 +180,21 @@ for thresh in thresholds:
     groupSummary['Group Number'] = np.unique(groups)
     groupSummary['Number of Atoms'] = numAtoms_list
     groupSummary['Number of Subjects'] = numSubs_list
-     
+    
+    numSubs_list = np.asarray(numSubs_list)
+    topGroups = len(np.where(numSubs_list>=12)[0]) 
+    threshold_dict = {'Threshold': thresh, 'Number of Groups': numGroups, 'Number of Top Groups': topGroups}
+    threshold_summary = threshold_summary.append(threshold_dict, ignore_index=True)
+
     #Save group summary dataframe
-    outputDir = '/media/NAS/lpower/CSC/results/u_' + str(u_thresh) + '_psd_' + str(psd_thresh) + '_groupSummary.csv'
+    outputDir = '/media/NAS/lpower/CSC/results/u_' + str(u_thresh) + '_v_' + str(v_thresh) + '_groupSummary.csv'
     groupSummary.to_csv(outputDir)
      
     #Save atomGroups to dataframe
-    outputDir = '/media/NAS/lpower/CSC/results/u_' + str(u_thresh) + '_psd_' + str(psd_thresh) + '_atomGroups.csv'
+    outputDir = '/media/NAS/lpower/CSC/results/u_' + str(u_thresh) + '_v_' + str(v_thresh) + '_atomGroups.csv'
     atomGroups.to_csv(outputDir)
 
+#Save threshold summary dataframe 
+#outputDir = '/media/NAS/lpower/CSC/results/thresholdSummary9.csv'
+#threshold_summary.to_csv(outputDir)
 
--- correlation_clustering_singleSub.py
+++ correlation_clustering_singleSub.py
@@ -2,126 +2,131 @@ import numpy as np
 import pandas as pd
 import pickle
 import matplotlib.pyplot as plt
+import scipy.signal as ss
+import os
 
-subjects = ['CC110037','CC110606','CC120166','CC120313','CC120727','CC121428','CC120640','CC210657','CC220518','CC220999',
-            'CC220107','CC222125','CC223286','CC310256','CC320089','CC320429','CC320621','CC321000','CC321431','CC321899',
-            'CC410287','CC420060','CC420157','CC420217','CC420383','CC420566','CC420729','CC510304','CC510438','CC520083',
-            'CC520215','CC520503','CC520597','CC520980','CC610061','CC610288','CC610576','CC620114','CC620264','CC620885',
-            'CC621642','CC710088','CC710342','CC710664','CC720290','CC721052','CC721504','CC722891','CC723395','CC221107']
+#subjects = ['CC110037','CC110606','CC120166','CC120313','CC120727','CC121428','CC120640','CC210657','CC220518','CC220999',
+#            'CC220107','CC222125','CC223286','CC310256','CC320089','CC320429','CC320621','CC321000','CC321431','CC321899',
+#            'CC410287','CC420060','CC420157','CC420217','CC420383','CC420566','CC420729','CC510304','CC510438','CC520083',
+#            'CC520215','CC520503','CC520597','CC520980','CC610061','CC610288','CC610576','CC620114','CC620264','CC620885',
+#            'CC621642','CC710088','CC710342','CC710664','CC720290','CC721052','CC721504','CC722891','CC723395','CC221107']
+
+subDir = '/media/NAS/lpower/CSC/results'
+subjects = next(os.walk(subDir))[1]
 
 #Set parameters
 u_thresh = 0.8
-psd_thresh = 0.8
+v_thresh = 0.8
 
 numGroups_list = []
+exclude_list = [] 
 
 for subID in subjects: 
     print(subID)
     
     #Read in cdl model from pickle file
     fileName = '/media/NAS/lpower/CSC/results/' + subID + '/CSCraw_0.5s_20atoms.pkl'
-    cdl_model, info, allZ, z_hat_ = pickle.load(open(fileName,"rb"))
-
-    atomGroups = pd.DataFrame(columns=['Subject ID', 'Atom number', 'Group number'])
-
-    atomNum = 0
-    groupNum = 0
-    unique = True
-
-    #Create a row in the dataframe for the first atom, placing it in group 0
-    groupDict = {'Subject ID': subID, 'Atom number': atomNum, 'Group number': groupNum}
-    atomGroups = atomGroups.append(groupDict, ignore_index=True)
-
-    #For each atom, checks if it is correlated to any atoms that have already been sorted and sorts accordingly
-    for atom in range(1, cdl_model.u_hat_.shape[0]):
-        atomNum = atom  
+    
+    if os.path.exists(fileName):
+        cdl_model, info, allZ, z_hat_ = pickle.load(open(fileName,"rb"))
         
-        max_corr = 0
-        max_group = 0 
+        atomGroups = pd.DataFrame(columns=['Subject ID', 'Atom number', 'Group number'])
 
-        #Loops through groups that have already been created and checks current atom's average  correlation
-        for group in range(0, groupNum+1):
-            gr_atoms = atomGroups[atomGroups['Group number'] == group]['Atom number'].tolist()
-            u_coefs = []
-            psd_coefs = []
-        
-            #for each atom in the group, calculate the correlation coefficients and average them
-            for atom2 in gr_atoms:
-                u_corrcoef = np.corrcoef(cdl_model.u_hat_[atom], cdl_model.u_hat_[atom2])[0,1]
-                u_coefs.append(u_corrcoef)
-
-                psd1 = np.abs(np.fft.rfft(cdl_model.v_hat_[atom], n=256)) ** 2
-                psd2 = np.abs(np.fft.rfft(cdl_model.v_hat_[atom2], n=256)) ** 2
-                psd_corrcoef = np.corrcoef(psd1, psd2)[0,1]
-                psd_coefs.append(psd_corrcoef)
-
-            #average across u and psd correlation coefficients 
-            u_coefs = abs(np.asarray(u_coefs))
-            avg_u = np.mean(u_coefs)
-
-            psd_coefs = abs(np.asarray(psd_coefs)) 
-            avg_psd = np.mean(psd_coefs)
-        
-
-            #If U vector and PSD correlations are both high, sorts atom into that group
-            if (avg_u > u_thresh) & (avg_psd > psd_thresh):
-                unique = False 
-                if (avg_u + avg_psd) > max_corr:
-                    max_corr = (avg_u + avg_psd)
-                    max_group = group
-                
-        #If a similar atom is not found, creates a new group and assigns the current atom to that group
-        if (unique == False):
-            groupDict = {'Subject ID': subID, 'Atom number': atomNum, 'Group number': max_group}
-        
-        if (unique == True): 
-            groupNum = groupNum + 1
-            groupDict = {'Subject ID': subID, 'Atom number': atomNum, 'Group number': groupNum}
-        
+        atomNum = 0
+        groupNum = 0
         unique = True
 
-        #Append data for current atom to dataframe 
+        #Create a row in the dataframe for the first atom, placing it in group 0
+        groupDict = {'Subject ID': subID, 'Atom number': atomNum, 'Group number': groupNum}
         atomGroups = atomGroups.append(groupDict, ignore_index=True)
 
-    #Summary statistics for the current dataframe:
+        #For each atom, checks if it is correlated to any atoms that have already been sorted and sorts accordingly
+        for atom in range(1, cdl_model.u_hat_.shape[0]):
+            atomNum = atom  
+        
+            max_corr = 0
+            max_group = 0 
 
-    #Number of distinct groups 
-    groups = atomGroups['Group number'].tolist()
-    groups = np.asarray(groups)
-    numGroups = len(np.unique(groups))
-    numGroups_list.append(numGroups)
+            #Loops through groups that have already been created and checks current atom's average  correlation
+            for group in range(0, groupNum+1):
+                gr_atoms = atomGroups[atomGroups['Group number'] == group]['Atom number'].tolist()
+                u_coefs = []
+                v_coefs = []
+        
+                #for each atom in the group, calculate the correlation coefficients and average them
+                for atom2 in gr_atoms:
+                    u_corrcoef = np.corrcoef(cdl_model.u_hat_[atom], cdl_model.u_hat_[atom2])[0,1]
+                    u_coefs.append(u_corrcoef)
 
-    #Number of atoms per group
-    numAtoms_list = []
+                    v_corrcoef = np.max(ss.correlate(cdl_model.v_hat_[atom],cdl_model.v_hat_[atom2]))
+                    v_coefs.append(v_corrcoef)
 
-    for un in np.unique(groups):
-        numAtoms = len(np.where(groups==un)[0])
-        numAtoms_list.append(numAtoms)
+                #average across u and psd correlation coefficients 
+                u_coefs = abs(np.asarray(u_coefs))
+                avg_u = np.mean(u_coefs)
 
-    numAtoms_list = np.asarray(numAtoms_list)
-    meanAtoms = np.mean(numAtoms_list)
-    stdAtoms = np.std(numAtoms_list)
+                v_coefs = abs(np.asarray(v_coefs)) 
+                avg_v = np.mean(v_coefs)
 
-    print("Number of groups:")
-    print(numGroups)
+                #If U vector and PSD correlations are both high, sorts atom into that group
+                if (avg_u > u_thresh) & (avg_v > v_thresh):
+                    unique = False 
+                    if (avg_u + avg_v) > max_corr:
+                        max_corr = (avg_u + avg_v)
+                        max_group = group
+                
+            #If a similar atom is not found, creates a new group and assigns the current atom to that group
+            if (unique == False):
+                groupDict = {'Subject ID': subID, 'Atom number': atomNum, 'Group number': max_group}
+        
+            if (unique == True): 
+                groupNum = groupNum + 1
+                groupDict = {'Subject ID': subID, 'Atom number': atomNum, 'Group number': groupNum}
+        
+            unique = True
 
-    print("Average number of atoms per group:")
-    print(str(meanAtoms) + " +/- " + str(stdAtoms))
+            #Append data for current atom to dataframe 
+            atomGroups = atomGroups.append(groupDict, ignore_index=True)
 
-    if numGroups<=12:
-        print('exclude')
+        #Summary statistics for the current dataframe:
 
-    groupSummary = pd.DataFrame(columns=['Group Number', 'Number of Atoms'])
-    groupSummary['Group Number'] = np.unique(groups)
-    groupSummary['Number of Atoms'] = numAtoms_list
+        #Number of distinct groups 
+        groups = atomGroups['Group number'].tolist()
+        groups = np.asarray(groups)
+        numGroups = len(np.unique(groups))
+        print(numGroups)
+        numGroups_list.append(numGroups)
 
-    #Save group summary dataframe
-    outputDir = '/media/NAS/lpower/CSC/results/' + subID + '/u_' + str(u_thresh) + '_psd_' + str(psd_thresh) + '_groupSummary.csv'
-    groupSummary.to_csv(outputDir)
+        #Number of atoms per group
+        numAtoms_list = []
 
-    #Save atomGroups to dataframe
-    outputDir = '/media/NAS/lpower/CSC/results/' + subID + '/u_' +  str(u_thresh) + '_psd_' + str(psd_thresh) + '_atomGroups.csv'
-    atomGroups.to_csv(outputDir)
+        for un in np.unique(groups):
+            numAtoms = len(np.where(groups==un)[0])
+            numAtoms_list.append(numAtoms)
+
+        numAtoms_list = np.asarray(numAtoms_list)
+        meanAtoms = np.mean(numAtoms_list)
+        stdAtoms = np.std(numAtoms_list)
+
+        #print("Average number of atoms per group:")
+        #print(str(meanAtoms) + " +/- " + str(stdAtoms))
+        
+        if numGroups<14:
+            exclude_list.append(subID)
+
+        groupSummary = pd.DataFrame(columns=['Group Number', 'Number of Atoms'])
+        groupSummary['Group Number'] = np.unique(groups)
+        groupSummary['Number of Atoms'] = numAtoms_list
+
+        #Save group summary dataframe
+        #outputDir = '/media/NAS/lpower/CSC/results/' + subID + '/u_' + str(u_thresh) + '_psd_' + str(psd_thresh) + '_groupSummary.csv'
+        #groupSummary.to_csv(outputDir)
+
+        #Save atomGroups to dataframe
+        #outputDir = '/media/NAS/lpower/CSC/results/' + subID + '/u_' +  str(u_thresh) + '_psd_' + str(psd_thresh) + '_atomGroups.csv'
+        #atomGroups.to_csv(outputDir)
+
+print(exclude_list)
 
 #Plot distribution of number of groups
 bins = np.arange(0,21)
